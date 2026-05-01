d982924f6e49338eaa8a92497a2ef95f97d71e24
Sleddie <JDelmuhametov@gmail.com>
2026-05-01 22:31:28 +0300

    Empty WinForms app

---
+++ BulkRequest.App.WinForms/MainForm.cs
@@ -0,0 +1,10 @@
+namespace BulkRequest.App.WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
